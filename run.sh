66148d5d21797504f042d9d16d0df45554d48203
Jonathan Moss <jwmoss88+github@gmail.com>
2020-12-27 01:14:01 -0500

    Update and rename run.ps1 to run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ docker build . -t scraper:1.0
 
 docker run -it --name scraper scraper:1.0
 
-docker rm -vf $(docker ps -a -q)
+#docker rm -vf $(docker ps -a -q)
 
-docker rmi -f $(docker images -a -q)
+#docker rmi -f $(docker images -a -q)
